f96819678804bcf49f9eb47e3c969a31adefc289
Jeremy Agrezio <jeremy.agrezio@orange.fr>
2024-07-13 17:46:00 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,8 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			//base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/planner-app'
 		}
 	}
 };
