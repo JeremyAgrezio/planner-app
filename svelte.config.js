b5ee68d15eaa20dbea11cae1baa21cb4b5489cc6
Jeremy Agrezio <jeremy.agrezio@orange.fr>
2024-07-13 15:43:36 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,8 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			//base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '/msm-report' : process.env.BASE_PATH
 			//base: process.argv.includes('dev') ? '/msm-report' : '/msm-report'
 			//base: process.env.NODE_ENV === 'production' ? '/msm-report' : '/msm-report',
 		}
